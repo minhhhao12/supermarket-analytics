e45603b1245906bb32b843f19d5e4898c93fef2d
minhhao <minhhao3006@gmail.com>
2026-05-21 22:41:33 +0700

    switch to object-oriented programming method

--- src/data_loader.py
+++ src/data_loader.py
@@ -2,33 +2,36 @@ import pandas as pd
 import os
 from pandas import DataFrame
 
+class DataLoader:
+    def __init__(self,pathname:str):
+        self.pathname=pathname
 
-def load_csv(path:str)->DataFrame:
-    if not path.lower().endswith('.csv'):
-        raise ValueError(f'Lỗi định dạng file')
-    if os.path.getsize(path)==0:
-        raise ValueError(f'File {path} có kích thước bằng 0')
-    try:
-        df=pd.read_csv(path)
-        if df.empty:
-            raise ValueError('File csv rỗng')
-        return df
-    except FileNotFoundError:
-        raise ValueError(f"Không tìm thấy file ở {path}")
-    except Exception as ex:
-        raise ValueError(f"Lỗi khi đọc file csv ở {path}")
+    def load_csv(self)->DataFrame:
+        if not self.pathname.lower().endswith('.csv'):
+            raise ValueError(f'Lỗi định dạng file')
+        if os.path.getsize(self.pathname)==0:
+            raise ValueError(f'File {self.pathname} có kích thước bằng 0')
+        try:
+            df=pd.read_csv(self.pathname)
+            if df.empty:
+                raise ValueError('File csv rỗng')
+            return df
+        except FileNotFoundError:
+            raise ValueError(f"Không tìm thấy file ở {self.pathname}")
+        except Exception as ex:
+            raise ValueError(f"Lỗi khi đọc file csv ở {self.pathname}")
 
-def load_excel(path:str)->DataFrame:
-    if not path.lower().endswith(('.xlsx','.xls')):
-        raise ValueError(f'Lỗi định dạng file')
-    if os.path.getsize(path) == 0:
-        raise ValueError(f'File {path} có kích thước bằng 0')
-    try:
-        df=pd.read_excel(path)
-        if df.empty:
-            raise ValueError('File excel rỗng')
-        return df
-    except FileNotFoundError:
-        raise ValueError(f"Không tìm thấy file ở {path}")
-    except Exception as ex:
-        raise ValueError(f"Lỗi khi đọc file excel ở {path}")
+    def load_excel(self)->DataFrame:
+        if not self.pathname.lower().endswith(('.xlsx','.xls')):
+            raise ValueError(f'Lỗi định dạng file')
+        if os.path.getsize(self.pathname) == 0:
+            raise ValueError(f'File {self.pathname} có kích thước bằng 0')
+        try:
+            df=pd.read_excel(self.pathname)
+            if df.empty:
+                raise ValueError('File excel rỗng')
+            return df
+        except FileNotFoundError:
+            raise ValueError(f"Không tìm thấy file ở {self.pathname}")
+        except Exception as ex:
+            raise ValueError(f"Lỗi khi đọc file excel ở {self.pathname}")
