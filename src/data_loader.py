80029262d085e6d98ba9cd0569a5a105dc0bc1a8
minhhhao12 <minhhao3006@gmail.com>
2026-05-23 12:42:06 +0700

    Refactor data loading methods for flexibility

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,37 +1,28 @@
-import pandas as pd
 import os
+import pandas as pd
 from pandas import DataFrame
 
 class DataLoader:
-    def __init__(self,pathname:str):
-        self.pathname=pathname
+    def __init__(self, pathname: str):
+        self.pathname = pathname
 
-    def load_csv(self)->DataFrame:
-        if not self.pathname.lower().endswith('.csv'):
-            raise ValueError(f'Lỗi định dạng file')
-        if os.path.getsize(self.pathname)==0:
-            raise ValueError(f'File {self.pathname} có kích thước bằng 0')
-        try:
-            df=pd.read_csv(self.pathname)
-            if df.empty:
-                raise ValueError('File csv rỗng')
-            return df
-        except FileNotFoundError:
-            raise ValueError(f"Không tìm thấy file ở {self.pathname}")
-        except Exception as ex:
-            raise ValueError(f"Lỗi khi đọc file csv ở {self.pathname}")
-
-    def load_excel(self)->DataFrame:
-        if not self.pathname.lower().endswith(('.xlsx','.xls')):
-            raise ValueError(f'Lỗi định dạng file')
+    def load_data(self) -> DataFrame:
+        if self.pathname.lower().endswith('.csv'):
+            read_file = pd.read_csv
+            file_type = 'csv'
+        elif self.pathname.lower().endswith(('.xlsx', '.xls')):
+            read_file = pd.read_excel
+            file_type = 'excel'
+        else:
+            raise ValueError('Lỗi định dạng file')
         if os.path.getsize(self.pathname) == 0:
             raise ValueError(f'File {self.pathname} có kích thước bằng 0')
         try:
-            df=pd.read_excel(self.pathname)
+            df = read_file(self.pathname)
             if df.empty:
-                raise ValueError('File excel rỗng')
+                raise ValueError(f'File {file_type} rỗng')
             return df
         except FileNotFoundError:
-            raise ValueError(f"Không tìm thấy file ở {self.pathname}")
+            raise ValueError(f'Không tìm thấy file ở {self.pathname}')
         except Exception as ex:
-            raise ValueError(f"Lỗi khi đọc file excel ở {self.pathname}")
+            raise ValueError(f'Lỗi khi đọc file {file_type} ở {self.pathname}')
