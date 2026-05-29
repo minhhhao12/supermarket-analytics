1eb2b8c361f0218e853773e42c9e7d7dfc26872f
minhhao <minhhao3006@gmail.com>
2026-05-30 00:21:29 +0700

    Hoàn thiện chức năng cơ bản cho trợ lý AI: - ai_assistant.py: Hoàn thiện chức năng gọi API, chat, đang hoàn thiện thêm tính năng function calling - data_processor.py: Thêm vài tính năng vào feature_engineering - analytics.py: Thêm những hàm phân tích

--- src/ai_assistant.py
+++ src/ai_assistant.py
@@ -5,6 +5,7 @@ import pandas as pd
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+from google.genai.types import GenerateContentResponse
 from googleapiclient import errors
 
 from analytics import Analytics
@@ -13,52 +14,82 @@ from data_processor import DataProcessor
 
 
 class AIAssistant:
-    def __init__(self):
+    def __init__(self,analytics_instance:Analytics=None):
         load_dotenv() #load file .env
-        # TODO 1: Đọc biến môi trường AI_API_KEY từ file .env
+        #Đọc biến môi trường AI_API_KEY từ file .env
         self.api_key=os.getenv("GEMINI_API_KEY")
-        # TODO 2: Kiểm tra nếu không có API Key thì báo lỗi (raise ValueError)
+        # Kiểm tra nếu không có API Key thì báo lỗi (raise ValueError)
         if not self.api_key:
             raise ValueError('Không tìm thấy API Key')
-        # TODO 3: Khởi tạo client kết nối (ví dụ: genai.Client(api_key=...))
+        #Khởi tạo client kết nối (ví dụ: genai.Client(api_key=...))
         self.client=genai.Client(api_key=self.api_key)
-        self.model='gemini-2.5-flash-lite'
+        self.model='gemma-4-26b-a4b-it'
+        self.analytics=analytics_instance
 
-    def _call_api(self, system_prompt: str, user_prompt: str):
+    def _call_api(self, system_prompt: str, user_prompt: str,tools:list=None)-> GenerateContentResponse | None:
         """
-        Hàm nội bộ: Dùng để gói gọn phần code gửi request lên API.
-        Giúp các hàm bên dưới không phải viết lại code gọi API nhiều lần.
+        Hàm nội bộ: Gửi request lên Gemini API và trả về đối tượng Response gốc.
         """
-        # TODO 1: Viết khối try-except để bắt lỗi mất mạng hoặc lỗi API.
-        # TODO 2: Truyền system_prompt (định hình vai trò AI) và user_prompt (câu lệnh + dữ liệu) vào API.
-        # TODO 3: Nhận kết quả văn bản trả về từ AI (response) và return.
         try:
-            config=genai.types.GenerateContentConfig(
+            config = genai.types.GenerateContentConfig(
                 system_instruction=system_prompt,
-                temperature=0.3
+                temperature=0.3,
+                tools=tools
             )
-            response=self.client.models.generate_content(
+            response = self.client.models.generate_content(
                 model=self.model,
                 contents=user_prompt,
                 config=config
             )
-            if response.text:
-                return response.text
-            else:
-                return 'AI không trả lời được câu hỏi'
+            return response
+
         except errors.HttpError as e:
             raise ValueError(f'Lỗi kết nối với máy chủ: {e}')
         except Exception as ex:
-            return f'Lỗi kết nối đến API: {ex}'
+            print(f'Lỗi kết nối đến API: {ex}')
+            return None
+
+    def get_branch_performance(self)->str:
+        """
+            Lấy bảng thống kê hiệu suất của các chi nhánh siêu thị, bao gồm doanh thu và số lượng đơn hàng.
+            Hàm này được gọi khi người dùng hỏi về doanh thu, so sánh hoặc tình hình kinh doanh của các chi nhánh.
+        """
+        if self.analytics: #kiểm tra xem thuộc tính self.analytics đã được khởi tạo (khác None) hay chưa.
+            df_branch=self.analytics.calculate_branch_performance()
+            return df_branch.to_markdown(index=False)
+        return 'Không có dữ liệu phân tích chi nhánh'
+    def get_products_trend(self)->str:
+        """
+        Lấy bảng dữ liệu về xu hướng ngành hàng bao gồm TOP ngành hàng bán chạy nhất và bán ế nhất.
+        Returns: Chuỗi văn bản chứa thông tin sản phẩm.
+        """
+        if self.analytics:
+            df_products_trend=self.analytics.get_top_and_bottom_products()
+            if isinstance(df_products_trend,tuple):
+                return f"TOP BÁN CHẠY:\n{df_products_trend[0].to_markdown()}\n\nTOP BÁN Ế:\n{df_products_trend[1].to_markdown()}"
+        return 'Không có dữ liệu phân tích sản phẩm.'
+    def get_branch_rating(self)->str:
+        """
+        Lấy bảng thống kế số điểm đánh giá trung bình ở các chi nhánh
+        Hàm này được gọi khi người dùng hỏi về điểm đánh giá ở các chi nhánh
+        return: Chuỗi văn bản chứa điểm đánh giá trung bình ở các chi nhánh
+        """
+        if self.analytics:
+            df_branch_rating=self.analytics.avg_rating_branches()
+            df_branch_rating=df_branch_rating.to_markdown(index=False)
+            return df_branch_rating
+        return 'Không có dữ liệu để phân tích'
+
+
+
+
+
+
 
     def summarize_monthly_performance(self, monthly_df: pd.DataFrame):
         """
         Tính năng: Tóm tắt báo cáo kinh doanh hàng tháng.
         """
-        # TODO 1: Chuyển bảng monthly_df thành dạng chuỗi văn bản (Gợi ý dùng .to_markdown(index=False)).
-        # TODO 2: Tạo `system_prompt` gán vai trò (Ví dụ: Chuyên gia phân tích dữ liệu).
-        # TODO 3: Tạo `user_prompt` lồng ghép chuỗi dữ liệu ở Bước 1. Yêu cầu AI chỉ ra tháng tốt nhất, tệ nhất và tóm tắt ngắn gọn.
-        # TODO 4: Gọi hàm self._call_api(...) và trả về kết quả.
         df = monthly_df.to_markdown(index=False)
         system_prompt = 'Bạn là chuyên gia Phân tích Kinh doanh (Business Analyst) của một chuỗi siêu thị lớn.'
         user_prompt=(f"""
@@ -72,16 +103,12 @@ class AIAssistant:
         """)
         return self._call_api(system_prompt, user_prompt)
 
-    def analyze_product_trends(self, top_best: pd.DataFrame, top_worst: pd.DataFrame) -> str | None:
+    def analyze_product_trends(self, top_best_top_worst:tuple) -> str | None:
         """
         Tính năng: Đưa ra đề xuất nhập hàng và cảnh báo hàng ế.
         """
-        # TODO 1: Chuyển 2 bảng top_best và top_worst thành dạng chuỗi văn bản.
-        # TODO 2: Tạo `system_prompt` gán vai trò (Ví dụ: Giám đốc chuỗi cung ứng).
-        # TODO 3: Tạo `user_prompt` lồng ghép 2 chuỗi dữ liệu. Yêu cầu AI đưa ra cảnh báo mặt hàng nào nên xả kho, mặt hàng nào nên nhập thêm.
-        # TODO 4: Gọi hàm self._call_api(...) và trả về kết quả.
-        best_product=top_best.to_markdown(index=False)
-        worst_product=top_worst.to_markdown(index=False)
+        best_product=top_best_top_worst[0].to_markdown(index=False)
+        worst_product=top_best_top_worst[1].to_markdown(index=False)
         system_prompt='Bạn là Giám đốc Quản lý Chuỗi cung ứng (Supply Chain Manager).'
         user_prompt=f"""
         Dưới đây là bảng dữ liệu TOP 5 ngành hàng BÁN CHẠY NHẤT:
@@ -95,27 +122,48 @@ class AIAssistant:
         2. Đề xuất những mặt hàng cần tăng cường làm việc với nhà cung cấp để nhập thêm.
         Hãy giải thích ngắn gọn lý do cho mỗi đề xuất.
         """
-        return self._call_api(system_prompt, user_prompt)
+        response=self._call_api(system_prompt,user_prompt)
+        return response.text
 
-    def chat_with_data(self, user_question: str, context_df: pd.DataFrame) -> str | None:
-        """
-        Tính năng: Chatbot trả lời câu hỏi trực tiếp của người dùng.
-        """
-        # TODO 1: Chuyển context_df thành chuỗi văn bản.
-        # TODO 2: Tạo `system_prompt` gán vai trò trợ lý hỏi đáp. (Rất quan trọng: Phải yêu cầu AI "Chỉ được trả lời dựa trên dữ liệu được cung cấp").
-        # TODO 3: Tạo `user_prompt` chứa chuỗi dữ liệu và câu hỏi `user_question` của người dùng.
-        # TODO 4: Gọi hàm self._call_api(...) và trả về kết quả
-        data=context_df.to_markdown(index=False)
-        system_prompt='Bạn là trợ lý AI thông minh hỗ trợ giám đốc siêu thị. Hãy trả lời câu hỏi một cách lịch sự, chính xác và DỰA HOÀN TOÀN VÀO DỮ LIỆU ĐƯỢC CUNG CẤP.'
-        user_prompt=f"""
-        Dữ liệu hiện tại của siêu thị:
-        {data}
-        
-        Câu hỏi của giám đốc: "{user_question}"
-        
-        Nếu câu hỏi không thể trả lời bằng dữ liệu trên, hãy nói: "Xin lỗi, dữ liệu hiện tại không đủ để tôi trả lời câu hỏi này."
-        """
+    def analyze_branch_performance(self,branch_stats:pd.DataFrame)->str|None:
+        system_prompt='Bạn là chuyên gia Phân tích Kinh doanh (Business Analyst) của một chuỗi siêu thị lớn.'
+        df_stats=branch_stats.to_markdown(index=False)
+        user_prompt = f"""
+                    Dưới đây là bảng dữ liệu doanh thu của các chi nhánh siêu thị:
+                    {df_stats}
+                    
+                    Dựa vào số liệu trên, hãy:
+                    1. Cho biết chi nhánh nào đang dẫn đầu doanh thu hiện tại và chi nhánh có doanh thu thấp nhất
+                    2. Cho biết chi nhánh nào đang có số lượng đơn hàng nhiều nhất và chi nhánh có số lượng đơn hàng thấp nhất
+                    3. Đề xuất 2-3 phương pháp để thu hút khách hàng hiệu quả
+                """
         return self._call_api(system_prompt,user_prompt)
+
+    def chat_with_data(self, user_question: str) -> str | None:
+        """
+        Tính năng: Chatbot thông minh tự động chọn hàm và phân tích dữ liệu cho Giám đốc.
+        """
+        system_prompt = (
+            "Bạn là trợ lý AI thông minh hỗ trợ giám đốc siêu thị. "
+            "Hãy sử dụng các công cụ được cung cấp để lấy dữ liệu chính xác trước khi trả lời."
+        )
+        tools_list = [self.get_products_trend, self.get_branch_performance,self.get_branch_rating]
+
+        try:
+            chat=self.client.chats.create(
+                model=self.model,
+                config=genai.types.GenerateContentConfig(
+                    system_instruction=system_prompt,
+                    temperature=0.3,
+                    tools=tools_list
+                )
+            )
+            response=chat.send_message(user_question)
+            return response.text
+
+        except Exception as ex:
+            print(f"Lỗi khi chat với dữ liệu: {ex}")
+            return "Xin lỗi, hệ thống gặp sự cố khi xử lý dữ liệu câu hỏi."
 
     def advise_on_predictions(self, forecast_df: pd.DataFrame) -> str:
         """
@@ -128,17 +176,25 @@ class AIAssistant:
         pass
 
 
-# Chạy thử nghiệm ngay trong file
 if __name__ == "__main__":
-    dataloader= DataLoader('../data/raw/supermarket_data_sales.csv')
-    df=dataloader.load_data()
-    dataprocess=DataProcessor(df)
-    df_cleaned=dataprocess.run_pipeline()
-    analytic=Analytics(df_cleaned.head(5))
-    mock_monthly_data=analytic.calculate_monthly_revenue_profit()
-    # 2. Kích hoạt Trợ lý
-    assistant = AIAssistant()
-    # 3. Gọi thử tính năng tóm tắt
-    print("--- ĐANG GỌI AI TÓM TẮT BÁO CÁO ---")
-    summary = assistant.summarize_monthly_performance(mock_monthly_data)
-    print(summary)
+    # Giả lập luồng nạp dữ liệu của bạn
+    dataloader = DataLoader('../data/raw/supermarket_data_sales.csv')
+    df = dataloader.load_data()
+    dataprocess = DataProcessor(df)
+    df_cleaned = dataprocess.run_pipeline()
+
+    # Khởi tạo đối tượng Analytics
+    analytic_instance = Analytics(df_cleaned.head(100))
+    # top_best_top_worst=analytic_instance.get_top_and_bottom_products()
+    assistant = AIAssistant(analytics_instance=analytic_instance)
+    # print(assistant.analyze_product_trends(top_best_top_worst))
+    # print("\n--- THỬ NGHIỆM CHATBOT FUNCTION CALLING ---")
+    #
+    # cau_hoi_1 = "Chi nhánh nào đang có doanh thu và số lượng đơn hàng tốt nhất vậy?"
+    # print(f"\nGiám đốc hỏi: {cau_hoi_1}")
+    # tra_loi_1 = assistant.chat_with_data(cau_hoi_1)
+    # print(f"AI trả lời:\n{tra_loi_1}")
+    cau_hoi_2=str(input())
+    print(f"\nGiám đốc hỏi: {cau_hoi_2}")
+    tra_loi_2 = assistant.chat_with_data(cau_hoi_2)
+    print(f"AI trả lời:\n{tra_loi_2}")
--- src/analytics.py
+++ src/analytics.py
@@ -1,23 +1,27 @@
-# TODO 1: Viết hàm tính tổng doanh thu và lợi nhuận theo từng tháng.
-# TODO 2: Viết hàm tìm top 5 mặt hàng bán chạy nhất và top 5 mặt hàng ế nhất.
-# TODO 3: Viết hàm tính toán hiệu suất của từng chi nhánh siêu thị.
 import pandas as pd
 from pandas import DataFrame
 
 class Analytics:
     def __init__(self,df:DataFrame):
         self.df=df
+
+    #: tính tổng doanh thu và lợi nhuận theo từng tháng.
+
     def calculate_monthly_revenue_profit(self) -> pd.DataFrame:
         monthly_stats = self.df.groupby('Month')[['Sales', 'Profit']].sum().reset_index()
         monthly_stats = monthly_stats.sort_values(by='Month')
         return monthly_stats
 
+
+    #tìm top 3 mặt hàng bán chạy nhất và top 3 mặt hàng ế nhất.
     def get_top_and_bottom_products(self) -> tuple:
         product_sales = self.df.groupby('Product line')['Quantity'].sum().reset_index()
-        top_bestseller = product_sales.sort_values(by='Quantity', ascending=False).head(5)
-        top_worst = product_sales.sort_values(by='Quantity', ascending=True).head(5)
+        top_bestseller = product_sales.sort_values(by='Quantity', ascending=False).head(3)
+        top_worst = product_sales.sort_values(by='Quantity', ascending=True).head(3)
         return top_bestseller, top_worst #tuple
 
+
+    #tính toán hiệu suất của từng chi nhánh siêu thị.
     def calculate_branch_performance(self) -> pd.DataFrame:
     # Hàm .agg() cho phép tính toán nhiều phép toán khác nhau trên các cột khác nhau cùng một lúc
         branch_stats = self.df.groupby('Branch').agg(
@@ -27,3 +31,56 @@ class Analytics:
      ).reset_index()
         branch_stats = branch_stats.sort_values(by='Total_Sales', ascending=False)
         return branch_stats
+
+    # Phân tích doanh thu và số lượng đơn hàng theo phương thức thanh toán
+    def calculate_revenue_and_number_of_orders_by_payment_method(self)->pd.DataFrame:
+        df=self.df.groupby('Payment').agg(
+            Total_Orders=('Invoice ID','count'),
+            Total_Sales=('Sales', 'sum'),
+            Total_Profit=('Profit', 'sum'),
+        )
+        df=df.sort_values(by='Total_Sales')
+        return df
+
+
+
+    # Phân tích doanh thu theo loại khách hàng (Customer type) và Giới tính (Gender)
+
+
+    # Phân tích khung giờ mua sắm cao điểm (Ví dụ: Sáng, Trưa, Chiều, Tối)
+    def analyze_shopping_hours(self)->pd.DataFrame:
+        df_shopping_hours=self.df.groupby('Hour').agg(
+            Total_Orders=('Invoice ID','count'),
+            Total_Sales=('Sales', 'sum'),
+            Total_Profit=('Profit', 'sum'),
+            avg_rating=('Rating','mean')
+        ).reset_index()
+        df_shopping_hours['avg_rating']=df_shopping_hours['avg_rating'].round(2)
+        df_shopping_hours=df_shopping_hours.sort_values(by='Total_Orders')
+        return df_shopping_hours
+
+    #: Doanh thu theo từng ngành
+    def calculate_revenue_by_product_line(self)->pd.DataFrame:
+        revenue_product_line_stats=self.df.groupby('Product line')[['Profit','Sales']].sum().reset_index()
+        return revenue_product_line_stats
+
+    #Điểm đánh giá trung bình ở các chi nhánh
+    def calculate_avg_rating_branches(self)->pd.DataFrame:
+        df_rating_branch=self.df.groupby('Branch')['Rating'].mean().reset_index()
+        df_rating_branch=df_rating_branch.sort_values(by='Rating')
+        return df_rating_branch
+
+    #: Điểm đánh giá theo từng ngành hàng
+    def get_rating_by_product_line(self)->pd.DataFrame:
+        df_rating_product_line=self.df.groupby('Product line')['Rating'].mean().reset_index()
+        df_rating_product_line=df_rating_product_line.sort_values(by='Rating')
+        return df_rating_product_line
+
+    # TODO: Sức mua trên mỗi đơn hàng (Quantity per Ticket): Trung bình một hóa đơn khách mua bao nhiêu sản phẩm? Mặt hàng đó là gì?
+    # Tương quan giữa các Ngành hàng (Cross-selling): Khách hàng mua ngành hàng A thì thường có xu hướng mua kèm ngành hàng nào khác không? (Ví dụ: Mua thực phẩm tươi sống có hay mua kèm gia vị không?).
+    # Tỷ suất lợi nhuận gộp (Gross Profit Margin): Ngành hàng nào đang "gánh" lợi nhuận cho siêu thị? Có ngành hàng doanh thu rất cao nhưng biên lợi nhuận lại quá mỏng không?
+    # Cấu trúc chi phí (Cost of Goods Sold - COGS Analysis): Tỷ trọng giá vốn trên doanh thu đổi theo từng tháng như thế nào? Xu hướng nhập hàng có đang bị đắt lên không?
+    # Năng suất xử lý hóa đơn theo khung giờ (Throughput): Khung giờ cao điểm hệ thống xử lý được bao nhiêu hóa đơn/phút? Có dấu hiệu bị nghẽn (quá tải) khiến điểm Rating giảm không?
+    # Biến động điểm số đánh giá (Rating Volatility): Tìm hiểu xem các điểm số thấp (1-3 sao) thường rơi vào chi nhánh nào, ngành hàng nào, hoặc phương thức thanh toán nào để tìm ra "vấn đề hệ thống" (Bottleneck).
+    # Mức độ trung thành (Loyalty Value): Nhóm khách hàng Member đóng góp bao nhiêu % vào tổng lợi nhuận so với nhóm khách vãng lai Normal?
+    # Sự khác biệt về hành vi theo giới tính (Gender Preference): Khách hàng Nam và Nữ có xu hướng chọn phương thức thanh toán khác nhau không? Họ thường gom nhóm mua hàng vào các khung giờ nào khác nhau?
